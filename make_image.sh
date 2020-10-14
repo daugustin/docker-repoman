6ae5000a62736707c899f2340e823f2eb44489c4
Daniel Augustin <hossie@hossie.de>
2020-10-14 21:44:59 +0200

    Try buildah v5

--- make_image.sh
+++ make_image.sh
@@ -11,7 +11,7 @@ buildcmd emerge-webrsync
 buildcmd mkdir -p /etc/portage/package.use /repo
 buildcmd bash -c 'echo "dev-vcs/git -perl" > /etc/portage/package.use/git'
 buildcmd emerge --quiet-build -q dev-vcs/git app-portage/repoman
-buildcmd bash -c 'source /etc/portage/make.conf && rm -v "${DISTDIR}"/*"'
+buildcmd bash -c 'source /etc/portage/make.conf && rm -v "${DISTDIR}"/*'
 buildcmd bash -c 'echo FEATURES="-ipc-sandbox -network-sandbox" >> /etc/portage/make.conf'
 
 buildah config --entrypoint "/usr/bin/repoman" "${c}"
