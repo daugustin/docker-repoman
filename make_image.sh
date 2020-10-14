cb365fa0b2453bfe4fb11d6d61af20e99e9c904d
Daniel Augustin <hossie@hossie.de>
2020-10-14 20:26:42 +0200

    Try buildah

---
+++ make_image.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -ex
+
+c=$(buildah from gentoo/stage3-amd64)
+
+buildcmd() {
+  buildah run --network host "${c}" -- "$@"
+}
+
+buildcmd emerge-webrsync
+buildcmd mkdir -p /etc/portage/package.use /repo
+buildcmd echo "dev-vcs/git -perl" > /etc/portage/package.use/git
+buildcmd emerge --quiet-build -q dev-vcs/git app-portage/repoman
+buildcmd bash -c "source /etc/portage/make.conf && rm \"${DISTDIR}\"/*"
+buildcmd echo 'FEATURES="-ipc-sandbox -network-sandbox"' >> /etc/portage/make.conf
+
+buildah config --entrypoint "/usr/bin/repoman" "${c}"
+buildah config --workingdir "/repo" "${c}"
+
+buildah commit --squash --rm "${c}" "ghcr.io/daugustin/repoman:latest"
